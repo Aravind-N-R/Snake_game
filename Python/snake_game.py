101c17640bb591056a01248cd09a7546ccf35894
Aravind N R <155415467+Aravind-N-R@users.noreply.github.com>
2025-07-01 21:52:25 +0530

    Update snake_game.py
    Added pause function and score tally

--- Python/snake_game.py
+++ Python/snake_game.py
@@ -3,6 +3,10 @@ import time
 import random
 
 delay = 0.05
+point = 0
+max_point = 0
+paused = False
+last_direction = "stop"
 
 #setting up the screen
 w = turtle.Screen()
@@ -28,8 +32,42 @@ snake.penup()
 snake.goto(0,0)
 snake.direction = "stop"
 
+#Scoring
+score = turtle.Turtle()
+score.speed = 0
+score.shape("square")
+score.color("#1AFF00")
+score.penup()
+score.hideturtle()
+score.goto(0, 300)
+score.write("Score: 0 High score: 0", align="center", font = ('Arial', 24, "normal"))
+
+game_over = turtle.Turtle()
+game_over.speed = 0
+game_over.shape("square")
+game_over.color("#1AFF00")
+game_over.penup()
+game_over.hideturtle()
+game_over.goto(0, 100)
+
+
+
 #snake body
 body = []
+
+
+#pause
+def pause():
+    global paused, last_direction
+    paused = not paused
+    if paused:
+        last_direction = snake.direction
+        snake.direction = "stop"
+    else:
+        snake.direction = last_direction
+
+
+#snake movement
 
 def move_up():
     if snake.direction != "down":
@@ -51,15 +89,23 @@ def move_right():
 def movement():
     if snake.direction == "up":
         snake.sety(snake.ycor()+20)
+        game_over.clear()
+        food.showturtle()
     
     if snake.direction == "down":
         snake.sety(snake.ycor()-20)
+        game_over.clear()
+        food.showturtle()
 
     if snake.direction == "right":
         snake.setx(snake.xcor()+20)
+        game_over.clear()
+        food.showturtle()
 
     if snake.direction == "left": 
         snake.setx(snake.xcor()-20)
+        game_over.clear()
+        food.showturtle()
 
 #keyboard input
 w.listen()
@@ -67,10 +113,13 @@ w.onkeypress(move_up, "w")
 w.onkeypress(move_down, "s")
 w.onkeypress(move_left, "a")
 w.onkeypress(move_right, "d")
+w.onkeypress(pause, "r")
 
 #ending game
 def endgame():
+    global point, max_point
     time.sleep(1)
+    
     snake.goto(0,0)
     snake.direction = "stop"
         
@@ -78,10 +127,19 @@ def endgame():
         b.goto(10000, 10000)
         
     body.clear()
+    food.hideturtle()
+    game_over.write("GAME OVER\n Move to start new game", align="center", font = ('Arial', 30, "bold"))
+    point = 0
+    score.clear()
+    score.write("Score: {} High score: {}".format(point, max_point), align = "center", font = ("Arial", 24, "normal"))
 
 #game loop
 while(True):
     w.update()
+    
+    if paused:
+        time.sleep(delay)
+        continue
 
     #collison with border
     if(snake.xcor()<-630 or snake.xcor()>630 or snake.ycor()>350 or snake.ycor()<-350):
@@ -91,6 +149,13 @@ while(True):
         x = random.randint(-630 , 630)
         y = random.randint(-350, 350)
         food.goto(x, y)
+        point += 1
+        if point > max_point:
+            max_point = point
+        
+        score.clear()
+        score.write("Score: {} High score: {}".format(point, max_point), align = "center", font = ("Arial", 24, "normal"))
+
         #adding bodies
         new_body = turtle.Turtle()
         new_body.speed(0)
